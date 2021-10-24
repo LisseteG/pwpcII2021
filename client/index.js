3b9018a2b04637e5c66948b03e4e4f25f569080a
LisseteG <liesskha98@gmail.com>
2021-10-23 20:37:34 -0500

    Materialize CSS

--- client/index.js
+++ client/index.js
@@ -1,26 +1,26 @@
 import './stylesheets/style.css';
 import './stylesheets/mystyle.css';
 
-console.log('Webpack working!');
+//console.log('Webpack working!');
 // Default parameters están disponibles solamente en ES6/2015
-let show = (m = 'Hot module replacement working ') => {
-  alert(m);
-};
+//let show = (m = 'Hot module replacement working ') => {
+//alert(m);
+//};
 
-show();
+//show();
 
-function resolveAfter25Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
+//function resolveAfter25Seconds() {
+//return new Promise((resolve) => {
+//setTimeout(() => {
+//resolve('resolved');
+//}, 2000);
+//});
+//}
 
-async function asyncCall() {
-  console.log('Calling an async function');
-  const result = await resolveAfter25Seconds();
-  console.log(result);
-}
+//async function asyncCall() {
+//console.log('Calling an async function');
+//const result = await resolveAfter25Seconds();
+//console.log(result);
+//}
 
-asyncCall();
+//asyncCall();-->
